00c85146216b9c1293ca24dd915fa70ea49add85
Breta Osod <bretaosodo@gmail.com>
2026-05-30 09:14:22 +0300

    Updated the kakfa broker code and removed broker and controller 3

---
+++ kafka/kafka_producers_raw/run_all_raw.py
@@ -0,0 +1,48 @@
+# run_all_raw_topics.py
+
+import subprocess
+import signal
+import time
+
+processes = []
+
+scripts = [
+    "visa_raw_producer.py",
+    "mastercard_raw_producer.py",
+    "mpesa_raw_producer.py",
+    "pesapal_raw_producer.py",
+    "flutterwave_raw_producer.py"
+]
+
+
+def shutdown(signum=None, frame=None):
+    print("\nStopping all producers...")
+
+    for process in processes:
+        process.terminate()
+
+    for process in processes:
+        process.wait()
+
+    print("All producers stopped.")
+    exit(0)
+
+
+if __name__ == "__main__":
+
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
+
+    print("Starting all raw topic producers...\n")
+
+    for script in scripts:
+        process = subprocess.Popen(["python", script])
+        processes.append(process)
+        print(f"Started {script}")
+
+    try:
+        while True:
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        shutdown()
